fe450ff33addf264bec076bd257bdd42db9abd8d
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-06-14 00:49:48 +0800

    update test

--- src/test/java/chaos/unity/nenggao/SimpleTest.java
+++ src/test/java/chaos/unity/nenggao/SimpleTest.java
@@ -13,12 +13,12 @@ public class SimpleTest {
         FileReportBuilder.sourceFile(testFile)
                 .relativePath(testFile.getParentFile().toPath())
                 .characterSet(CharacterSet.UNICODE)
-                .warning(Span.multipleLine(1, 0, 6, 0), "Test1")
-                .label(Span.multipleLine(1, 0, 3, 8), "LOL").color(Attribute.RED_TEXT()).build()
-                .label(Span.multipleLine(2, 0,5, 0), "KEK").color(Attribute.BRIGHT_MAGENTA_TEXT()).build()
-                .label(Span.singleLine(1, 6, 11), "L").color(Attribute.BRIGHT_CYAN_TEXT()).build()
-                .label(Span.singleLine(3, 0, 4), "impl dude").color(Attribute.BRIGHT_CYAN_TEXT()).build()
-                .label(Span.multipleLine(4, 0, 6, 0), "kek").build()
+                .warning(Span.multipleLine(1, 0, 6, 0), "This is yakou lang source code")
+                .label(Span.multipleLine(1, 0, 3, 8), "They link together at compile time").color(Attribute.RED_TEXT()).build()
+                .label(Span.multipleLine(2, 0,5, 0), "IDK why I mark this section lol").color(Attribute.BRIGHT_MAGENTA_TEXT()).build()
+                .label(Span.singleLine(1, 6, 11), "The name of class").color(Attribute.BRIGHT_CYAN_TEXT()).build()
+                .label(Span.singleLine(3, 0, 4), "Implementation of class `Yakou`").color(Attribute.BRIGHT_CYAN_TEXT()).build()
+                .label(Span.multipleLine(4, 0, 6, 0), "Just some redundant empty lines").build()
                 .build()
                 .print(System.out);
     }
