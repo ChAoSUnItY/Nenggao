cc4c5e116deb1d8e689d415b0c95bf05081f0e87
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-06-14 02:31:27 +0800

    make SourceCache#INSTANCE public, bump version to 1.0.4

--- src/main/java/chaos/unity/nenggao/SourceCache.java
+++ src/main/java/chaos/unity/nenggao/SourceCache.java
@@ -8,9 +8,9 @@ import java.util.Map;
 import java.util.Objects;
 
 public final class SourceCache {
-    static final SourceCache INSTANCE = new SourceCache();
+    public static final SourceCache INSTANCE = new SourceCache();
 
-    final @NotNull Map<@NotNull File, @NotNull Source> sources = new HashMap<>();
+    private final @NotNull Map<@NotNull File, @NotNull Source> sources = new HashMap<>();
 
     public @NotNull Source getOrAdd(@NotNull File sourceFile) {
         return Objects.requireNonNull(sources.computeIfAbsent(sourceFile, file -> Source.fromFile(sourceFile)));
