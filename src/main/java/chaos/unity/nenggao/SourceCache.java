33b9bd5ba006a58ea5a7edc3bedb7d698d8b8827
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-11-04 23:22:13 +0800

    Implement HashMap on SourceCache

--- src/main/java/chaos/unity/nenggao/SourceCache.java
+++ src/main/java/chaos/unity/nenggao/SourceCache.java
@@ -4,15 +4,12 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
-public final class SourceCache {
+public final class SourceCache extends HashMap<@NotNull File, @NotNull Source> {
     public static final SourceCache INSTANCE = new SourceCache();
 
-    private final @NotNull Map<@NotNull File, @NotNull Source> sources = new HashMap<>();
-
     public @NotNull Source getOrAdd(@NotNull File sourceFile) {
-        return Objects.requireNonNull(sources.computeIfAbsent(sourceFile, file -> Source.fromFile(sourceFile)));
+        return Objects.requireNonNull(computeIfAbsent(sourceFile, file -> Objects.requireNonNull(Source.fromFile(sourceFile))));
     }
 }
